4a6c6e37d3afdc6542eef892c710034a09af7035
Sanjeev choubey <Sanjeevchoubey215@gmail.com>
2023-01-22 00:40:42 +0530

    redux-thunk for async request

--- redux-app/src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import { productReducer, selectedProductReducers } from "./productReducer";
-
-const reducers = combineReducers({
-    allProducts: productReducer,
-    product: selectedProductReducers,
-});
-
-export default reducers;
